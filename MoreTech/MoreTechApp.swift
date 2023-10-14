bab0bb3a30bf030b52c5bd3c2c07917e026e0950
d.semischastnov <d.semischastnov@cardsmobile.ru>
2023-10-14 12:11:48 +0300

    Initial Commit

---
+++ MoreTech/MoreTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoreTechApp.swift
+//  MoreTech
+//
+//  Created by Danila Semischastnov on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoreTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
